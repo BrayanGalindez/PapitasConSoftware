5d98175e754a3430d54c56499e1b478ccc1b2fae
BrayanGalindez <bgalindez@unal.edu.co>
2023-05-26 18:05:16 -0500

    Update SignIn.jsx

--- bogota_taste/src/components/SignIn.jsx
+++ bogota_taste/src/components/SignIn.jsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import '../styles/SignIn.css';
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/login', { email, password });
+      // Aquí puedes manejar la respuesta del backend, como guardar un token de sesión en el almacenamiento local (localStorage) o realizar acciones adicionales.
+      console.log(response.data);
+    } catch (error) {
+      // Aquí puedes manejar el error en caso de que la solicitud falle.
+      console.error(error);
+    }
+  };
+
   return (
     <div className="form-container">
       <h2>Iniciar sesión</h2>
-      <form>
+      <form onSubmit={handleSignIn}>
         <div className="form-group">
           <label htmlFor="email">Correo electrónico</label>
-          <input type="email" id="email" name="email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Contraseña</label>
-          <input type="password" id="password" name="password" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button type="submit">Iniciar sesión</button>
       </form>
-      <p>¿No tienes una cuenta? <Link to="/registrar">Regístrate aquí</Link></p>
+      <p>
+        ¿No tienes una cuenta? <Link to="/registrar">Regístrate aquí</Link>
+      </p>
     </div>
   );
 };
