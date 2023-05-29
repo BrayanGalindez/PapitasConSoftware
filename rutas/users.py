c4864300278be5df978eb0580f1f952b4bba9d60
dasayo <dsepulvedaa@unal.edu.co>
2023-05-29 17:40:42 -0500

    login

--- rutas/users.py
+++ rutas/users.py
@@ -2,7 +2,10 @@ from fastapi import APIRouter, Response
 from schemas.userSchema import userEntity, usersEntity
 from config.db import coleccionUser
 from models.userModel import User
+from models.loginModel import loginModel
 from bcrypt import  hashpw, gensalt
+import auth.jwt_handler as jwth
+from fastapi import HTTPException
 from starlette.status import HTTP_204_NO_CONTENT, HTTP_201_CREATED, HTTP_500_INTERNAL_SERVER_ERROR
 
 
@@ -22,6 +25,7 @@ async def findAllUsers():
     return f'status: ok, datos: {usuarios}'
 
 
+
 def imprimirUsuario(user: dict):
     return (f"Nombre: {user['nombre']}\nApellido: {user['apellido']}\nDireccion: {user['direccion']}\nCorreo: {user['correo']}\nComentarios: {user['comentarios']}\nRestaurantes añadidos: {user['resAnadidos']}\n Teléfono: {user['telefono']}")
 
@@ -34,6 +38,7 @@ async  def findOneUser(id:int):
 @router.post('/')
 async def insertOneUser(user:User):
     usuarioNuevo = dict(user)
+    print(usuarioNuevo['passw'])
     usuarioNuevo['passw'] = str(hashpw(bytes(usuarioNuevo['passw']),salt))
 
     id = coleccionUser.insert_one(usuarioNuevo).inserted_id
@@ -61,3 +66,22 @@ def deleteUser(id: int):
     except:
         return f'Usuario no encontrado, error: {Response(status_code=HTTP_500_INTERNAL_SERVER_ERROR)}'
 
+@router.post('/login')
+async def login(user: loginModel):
+    try:
+        userNuevo = dict(user)
+        print(userNuevo['passw'])
+        print(userNuevo['correo'])
+        userNuevo['passw'] = str(hashpw(bytes(userNuevo['passw']),salt))
+        usuario = userEntity(coleccionUser.find_one({"correo": userNuevo['correo']}))
+        print(usuario['correo'])
+        print(usuario['passw'])
+        print(userNuevo['passw'])
+        if usuario['passw'] == userNuevo['passw']:
+            token = jwth.signJWT(usuario['id'])
+            return {'token': token}
+        else:
+            raise HTTPException(status_code=401, detail="Correo o contraseña incorrectos")
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=401, detail="Correo o contraseña incorrectos")
