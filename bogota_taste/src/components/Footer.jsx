85cc782385eff418d4753bebc6695a0a97bc236d
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-08 13:56:15 -0500

    Update Footer.jsx

--- bogota_taste/src/components/Footer.jsx
+++ bogota_taste/src/components/Footer.jsx
@@ -1,46 +1,43 @@
 import React from 'react';
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIcon from '@mui/icons-material/Phone';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import { FaEnvelope, FaPhone } from 'react-icons/fa';
+import { RiInstagramFill, RiFacebookFill, RiTwitterFill } from 'react-icons/ri';
+import logo from '../utils/logo.jpg';
 import '../styles/Footer.css';
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-          <div className="footer-container">
-            <div className="footer-column">
-              <div className="footer-logo">
-                <img src={'../utils/logo.png'} alt="Logo" />
-              </div>
-              <div className="footer-contact">
-                <div className="footer-contact-item">
-                  <EmailIcon />
-                  <span>example@example.com</span>
-                </div>
-                <div className="footer-contact-item">
-                  <PhoneIcon />
-                  <span>(123) 456-7890</span>
-                </div>
-              </div>
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-column">
+          <div className="footer-logo">
+            <img src={logo} alt="Logo" />
+          </div>
+          <div className="footer-contact">
+            <div className="footer-contact-item">
+              <FaEnvelope />
+              <span>example@example.com</span>
             </div>
-            <div className="footer-column">
-              <div className="footer-social">
-                <InstagramIcon />
-                <FacebookIcon />
-                <TwitterIcon />
-              </div>
+            <div className="footer-contact-item">
+              <FaPhone />
+              <span>(123) 456-7890</span>
             </div>
           </div>
-          <div className="footer-bottom">
-            <span className="footer-copyright">
-              &copy; {new Date().getFullYear()} Your Website. All rights reserved.
-            </span>
+        </div>
+        <div className="footer-column">
+          <div className="footer-social">
+            <RiInstagramFill />
+            <RiFacebookFill />
+            <RiTwitterFill />
           </div>
-        </footer>
-      );
-
-}
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <span className="footer-copyright">
+          &copy; {new Date().getFullYear()} Your Website. All rights reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
