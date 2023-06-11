a85ade89cf09f6fa8fd7cfd671bf40a1f59bb17e
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-11 17:51:08 -0500

    Update App.js

--- bogota_taste/src/App.js
+++ bogota_taste/src/App.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
 import SignUp from './components/Home/SignUp'; 
 import SignIn from './components/Home/SignIn';
 import RestaurantCard from './components/Restaurant/RestaurantCard';
-import RestaurantQueryView from './components/Home/RestaurantQueryView';
 import DeleteRestaurant from './components/Restaurant/DeleteRestaurant';
 import EditRestaurant from './components/Restaurant/EditRestaurant';
-import FavoritesView from './components/Home/RestaurantFavorites';
+import RestaurantFavorites from './components/Home/RestaurantFavorites';
 import AddReviewView from './components/Review/AddReviewView';
-import HomePage2 from './components/Home/Home';
+import HomePage from './components/Home/Home';
 import UserAccount from './components/User/UserAccount';
+import TopBar from './components/Home/TopBar';
+
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" >
-          <Route path="inicio" element={<HomePage2/>}/>
-          <Route path="iniciar" element={<SignIn/>}/>
-          <Route path="registrar" element={<SignUp/>}/>
-          <Route path="lista" element={<RestaurantCard/>}/>
-          <Route path="consultar" element={<RestaurantQueryView/>}/>
-          <Route path="eliminar" element={<DeleteRestaurant/>}/>
-          <Route path="editar" element={<EditRestaurant/>}/>
-          <Route path="favoritos" element={<FavoritesView/>}/>
-          <Route path="reseña" element={<AddReviewView/>}/>
-          <Route path="cuenta" element={<UserAccount/>}/>
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" >
+            <Route path="inicio" element={<HomePage/>}/>
+            <Route path="iniciar" element={<SignIn/>}/>
+            <Route path="registrar" element={<SignUp/>}/>
+            <Route path="lista" element={<RestaurantCard/>}/>
+            <Route path="eliminar" element={<DeleteRestaurant/>}/>
+            <Route path="editar" element={<EditRestaurant/>}/>
+            <Route path="favoritos" element={<RestaurantFavorites/>}/>
+            <Route path="reseña" element={<AddReviewView/>}/>
+            <Route path="cuenta" element={<UserAccount/>}/>
+            <Route path="barra" element={<TopBar/>}/>
+          </Route>
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
