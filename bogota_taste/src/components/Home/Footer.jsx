11296004bbabfeed7982c013ff831a931fa7c1fc
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-11 17:51:38 -0500

    Update Footer.jsx

--- bogota_taste/src/components/Home/Footer.jsx
+++ bogota_taste/src/components/Home/Footer.jsx
@@ -1,40 +1,36 @@
 import React from 'react';
 import { FaEnvelope, FaPhone } from 'react-icons/fa';
 import { RiInstagramFill, RiFacebookFill, RiTwitterFill } from 'react-icons/ri';
-import logo from '../../utils/logo.jpg';
-import '../../styles/Footer.css';
+import '../../styles/Home/Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <div className="footer-column">
-          <div className="footer-logo">
-            <img src={logo} alt="Logo" />
+        <div className="footer-contact">
+          <div className="footer-contact-item">
+            <FaEnvelope className="footer-icon" />
+            <span>example@example.com</span>
           </div>
-          <div className="footer-contact">
-            <div className="footer-contact-item">
-              <FaEnvelope />
-              <span>example@example.com</span>
-            </div>
-            <div className="footer-contact-item">
-              <FaPhone />
-              <span>(123) 456-7890</span>
-            </div>
+          <div className="footer-contact-item">
+            <FaPhone className="footer-icon" />
+            <span>(123) 456-7890</span>
           </div>
         </div>
-        <div className="footer-column">
-          <div className="footer-social">
-            <RiInstagramFill />
-            <RiFacebookFill />
-            <RiTwitterFill />
-          </div>
+        <div className="footer-social">
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <RiInstagramFill className="footer-icon" />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+            <RiFacebookFill className="footer-icon" />
+          </a>
+          <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+            <RiTwitterFill className="footer-icon" />
+          </a>
         </div>
       </div>
       <div className="footer-bottom">
-        <span className="footer-copyright">
-          &copy; {new Date().getFullYear()} Your Website. All rights reserved.
-        </span>
+        <span>&copy; {new Date().getFullYear()} Your Website. All rights reserved.</span>
       </div>
     </footer>
   );
