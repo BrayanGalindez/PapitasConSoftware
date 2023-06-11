352119bbe432ac85ad11eaaab6ac6cbe96a1da62
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-11 17:51:26 -0500

    Update Filter.jsx

--- bogota_taste/src/components/Home/Filter.jsx
+++ bogota_taste/src/components/Home/Filter.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../../styles/Home/Filter.css';
 
 const Filter = ({ handleFilter }) => {
   const [selectedOption, setSelectedOption] = useState('');
@@ -10,34 +11,34 @@ const Filter = ({ handleFilter }) => {
 
   return (
     <div className="filter-container">
-      <h3>Filter Restaurants</h3>
+      <h3 className="filter-title">Filtrar por:</h3>
       <div className="filter-options">
-        <label>
+        <label className="filter-option">
           <input
             type="radio"
             value="rating"
             checked={selectedOption === 'rating'}
             onChange={handleOptionChange}
           />
-          Rating
+          <span className="filter-label">Calificación</span>
         </label>
-        <label>
+        <label className="filter-option">
           <input
             type="radio"
             value="distance"
             checked={selectedOption === 'distance'}
             onChange={handleOptionChange}
           />
-          Distance
+          <span className="filter-label">Distancia</span>
         </label>
-        <label>
+        <label className="filter-option">
           <input
             type="radio"
             value="price"
             checked={selectedOption === 'price'}
             onChange={handleOptionChange}
           />
-          Price
+          <span className="filter-label">Precio</span>
         </label>
       </div>
     </div>
