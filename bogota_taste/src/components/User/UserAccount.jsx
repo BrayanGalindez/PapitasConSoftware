ec24282ef24ddf274e52d9ba69748b624b993ead
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-11 17:52:55 -0500

    Update UserAccount.jsx

--- bogota_taste/src/components/User/UserAccount.jsx
+++ bogota_taste/src/components/User/UserAccount.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import '../../styles/User/UserAccount.css'
+import '../../styles/User/UserAccount.css';
 import TopBar from '../Home/TopBar';
 import Footer from '../Home/Footer';
 import UserData from '../User/UserData';
 
-const UserAccount = () => {
+const UserAccount = ({ userId }) => {
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     // Fetch user data from the API
     const fetchUserData = async () => {
       try {
-        const response = await axios.get('/api/user/id');
+        const response = await axios.get(`http://127.0.0.1:8000/users/${userId}`);
         setUserData(response.data);
       } catch (error) {
         console.error('Error fetching user data:', error);
@@ -20,12 +20,14 @@ const UserAccount = () => {
     };
 
     fetchUserData();
-  }, []);
+  }, [userId]);
 
   return (
     <div>
-      <TopBar />
-      {userData && <UserData data={userData} />}
+      <TopBar showSearch={false} />
+      <div className="user-account-container">
+        {userData && <UserData data={userData} />}
+      </div>
       <Footer />
     </div>
   );
