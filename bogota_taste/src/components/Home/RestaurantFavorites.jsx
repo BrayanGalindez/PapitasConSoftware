089fde1663bc4b6936c861375bf6649bde8c18cb
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-21 00:18:40 -0500

    Update RestaurantFavorites.jsx

--- bogota_taste/src/components/Home/RestaurantFavorites.jsx
+++ bogota_taste/src/components/Home/RestaurantFavorites.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import '../../styles/Home/Footer.css';
 import '../../styles/Home/TopBar.css';
@@ -6,22 +6,30 @@ import '../../styles/Home/RestaurantFavorites.css';
 import TopBar from './TopBar.jsx';
 import Footer from './Footer.jsx';
 import RestaurantCard from '../Restaurant/RestaurantCard.jsx';
-
+import { AuthContext } from '../../AuthContext';
 
 const RestaurantFavorites = () => {
+  const { userId } = useContext(AuthContext);
   const [favoriteRestaurants, setFavoriteRestaurants] = useState([]);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
 
   useEffect(() => {
-    fetchFavoriteRestaurants();
-  }, []);
+    if (userId) {
+      fetchFavoriteRestaurants();
+    }
+  }, [userId]);
 
   const fetchFavoriteRestaurants = async () => {
     try {
-      const response = await axios.get('/api/favorites');
-      setFavoriteRestaurants(response.data);
+      const response = await axios.get(`http://127.0.0.1:8000/users/${userId}/favoritos`);
+      setFavoriteRestaurants(response.data.favoritos);
     } catch (error) {
       console.error('Error fetching favorite restaurants:', error);
     }
+  };
+
+  const handleViewMore = (restaurant) => {
+    setSelectedRestaurant(restaurant);
   };
 
   const removeFavoriteRestaurant = async (restaurantId) => {
@@ -39,15 +47,24 @@ const RestaurantFavorites = () => {
     <div className="restaurant-favorites">
       <TopBar />
       <h2 className="restaurant-favorites-heading">Tus restaurantes favoritos</h2>
-      {favoriteRestaurants.length > 0 && (
+      {favoriteRestaurants.length > 0 ? (
         <div className="restaurant-favorites-list">
-          {favoriteRestaurants.map((restaurant) => (
+          {favoriteRestaurants.map((restaurant, index) => (
             <RestaurantCard
-              key={restaurant.id}
-              restaurant={restaurant}
-              onRemove={() => removeFavoriteRestaurant(restaurant.id)}
+            key={index}
+            name={restaurant}
             />
           ))}
+        </div>
+      ) : (
+        <p>No tienes restaurantes favoritos.</p>
+      )}
+      {selectedRestaurant && (
+        <div className="restaurant-details">
+          {/* Mostrar información completa del restaurante */}
+          <h2>{selectedRestaurant.nombre}</h2>
+          <p>{selectedRestaurant.description}</p>
+          {/* ... otras opciones y funcionalidades */}
         </div>
       )}
       <Footer />
