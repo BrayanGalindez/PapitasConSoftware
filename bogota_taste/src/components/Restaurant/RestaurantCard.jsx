3fac710c9113af9d086387f97dc048ee4a49dce7
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-21 00:19:45 -0500

    Update RestaurantCard.jsx

--- bogota_taste/src/components/Restaurant/RestaurantCard.jsx
+++ bogota_taste/src/components/Restaurant/RestaurantCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import '../../styles/Restaurant/RestaurantCard.css'
-const RestaurantCard = ({ name, image, description }) => {
+import '../../styles/Restaurant/RestaurantCard.css';
+
+const RestaurantCard = ({ name, image, description, direccion, onViewMore }) => {
   return (
     <div className="restaurant-card">
       <img src={image} alt={name} />
-      <h3>{name}</h3>s
+      <h3>{name}</h3>
       <p>{description}</p>
-      <button>Ver más</button>
+      <h3>{direccion}</h3>
+      <button onClick={onViewMore}>Ver más</button>
     </div>
   );
 };
