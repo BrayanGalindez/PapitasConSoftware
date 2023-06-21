da2004b447a362eff0321eb80031569a38b222ec
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-21 16:44:06 -0500

    Update Home.jsx

--- bogota_taste/src/components/Home/Home.jsx
+++ bogota_taste/src/components/Home/Home.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, {useState, useContext  } from 'react';
 import '../../styles/Home/Home.css';
 import TopBar from './TopBar.jsx';
 import Footer from './Footer.jsx';
 import Filter from './Filter.jsx';
 import RestaurantPage from '../Restaurant/RestaurantPage';
 import CreateRestaurant from './CreateRestaurant.jsx';
+import { AuthContext } from '../../AuthContext.js'; 
 
 const HomePage = () => {
+  const { isAuthenticated } = useContext(AuthContext)
   const [showCreateRestaurant, setShowCreateRestaurant] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleOpenCreateRestaurant = () => {
     setShowCreateRestaurant(true);
@@ -25,11 +26,12 @@ const HomePage = () => {
         <Filter />
         <div className='restaurants'>
           <RestaurantPage />
-          {isLoggedIn && (
+          {isAuthenticated && (
             <button className='floating-button' onClick={handleOpenCreateRestaurant}>
               +
             </button>
           )}
+          
         </div>
       </div>
       <Footer />
