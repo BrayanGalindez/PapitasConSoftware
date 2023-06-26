a76bbcec51d33115e2d36b45326fc291d691c5eb
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-25 23:32:14 -0500

    Update AuthContext.js

--- bogota_taste/src/AuthContext.js
+++ bogota_taste/src/AuthContext.js
@@ -3,20 +3,44 @@ import React, { useState, createContext } from 'react';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userId, setUserId] = useState('');
-
+  const storedToken = localStorage.getItem('tokenLocal');
+  const storedUserId = localStorage.getItem('userIdLocal');
+  const [isAuthenticated, setIsAuthenticated] = useState(Boolean(storedToken));
+  const [restaurants, setRestaurants] = useState([]);
+  
   const handleAuth = () => {
     setIsAuthenticated(true);
+    localStorage.setItem('tokenLocal', storedToken);
+    localStorage.setItem('userIdLocal', storedUserId);
   };
 
   const handleLogout = () => {
     setIsAuthenticated(false);
-    localStorage.removeItem('token');
+    localStorage.removeItem('tokenLocal');
+    localStorage.removeItem('userIdLocal');
+  };
+
+  const updateRestaurants = (newRestaurants) => {
+    setRestaurants(newRestaurants);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, handleAuth, handleLogout, userId, setUserId }}>
+    <AuthContext.Provider
+      value={{
+        storedUserId,
+        isAuthenticated,
+        handleAuth,
+        handleLogout,
+        restaurants,
+        updateRestaurants,
+        setToken: (token) => {
+          localStorage.setItem('tokenLocal', token);
+        },
+        setUserId: (userId) => {
+          localStorage.setItem('userIdLocal', userId);
+        }
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
