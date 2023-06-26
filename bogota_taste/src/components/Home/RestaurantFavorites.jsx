13f13c14bedd7c74aa47a931a152836286198535
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-25 23:33:10 -0500

    Update RestaurantFavorites.jsx

--- bogota_taste/src/components/Home/RestaurantFavorites.jsx
+++ bogota_taste/src/components/Home/RestaurantFavorites.jsx
@@ -5,68 +5,73 @@ import '../../styles/Home/TopBar.css';
 import '../../styles/Home/RestaurantFavorites.css';
 import TopBar from './TopBar.jsx';
 import Footer from './Footer.jsx';
-import RestaurantCard from '../Restaurant/RestaurantCard.jsx';
+import RestaurantCardFavorites from '../Restaurant/RestaurantCardFavorites.jsx';
 import { AuthContext } from '../../AuthContext';
 
 const RestaurantFavorites = () => {
-  const { userId } = useContext(AuthContext);
+  const { storedUserId } = useContext(AuthContext);
   const [favoriteRestaurants, setFavoriteRestaurants] = useState([]);
   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
 
   useEffect(() => {
-    if (userId) {
+    const fetchFavoriteRestaurants = async () => {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/users/${storedUserId}/favoritos`);
+        setFavoriteRestaurants(response.data);
+      } catch (error) {
+        console.error('Error fetching favorite restaurants:', error);
+      }
+    };
+  
+    if (storedUserId) {
       fetchFavoriteRestaurants();
     }
-  }, [userId]);
+  }, [storedUserId]);
 
-  const fetchFavoriteRestaurants = async () => {
-    try {
-      const response = await axios.get(`http://127.0.0.1:8000/users/${userId}/favoritos`);
-      setFavoriteRestaurants(response.data);
-    } catch (error) {
-      console.error('Error fetching favorite restaurants:', error);
-    }
-  };
 
-  const handleViewMore = (restaurant) => {
-    setSelectedRestaurant(restaurant);
-  };
 
-  const removeFavoriteRestaurant = async (restaurantId) => {
-    try {
-      await axios.delete(`/api/favorites/${restaurantId}`);
-      setFavoriteRestaurants((prevRestaurants) =>
-        prevRestaurants.filter((restaurant) => restaurant.id !== restaurantId)
-      );
-    } catch (error) {
-      console.error('Error removing favorite restaurant:', error);
-    }
-  };
+  // const handleViewMore = (restaurant) => {
+  //   setSelectedRestaurant(restaurant);
+  // };
+
+  // const removeFavoriteRestaurant = async (restaurantId) => {
+  //   try {
+  //     await axios.delete(`/api/favorites/${restaurantId}`);
+  //     setFavoriteRestaurants((prevRestaurants) =>
+  //       prevRestaurants.filter((restaurant) => restaurant.id !== restaurantId)
+  //     );
+  //   } catch (error) {
+  //     console.error('Error removing favorite restaurant:', error);
+  //   }
+  // };
 
   return (
-    <div className="restaurant-favorites">
-      <TopBar />
-      <h2 className="restaurant-favorites-heading">Tus restaurantes favoritos</h2>
-      {favoriteRestaurants.length > 0 ? (
-        <div className="restaurant-favorites-list">
-          {favoriteRestaurants.map((restaurant, index) => (
-            <RestaurantCard
-            key={index}
-            name={restaurant}
-            />
-          ))}
-        </div>
-      ) : (
-        <p>No tienes restaurantes favoritos.</p>
-      )}
-      {selectedRestaurant && (
-        <div className="restaurant-details">
-          {/* Mostrar información completa del restaurante */}
-          <h2>{selectedRestaurant.nombre}</h2>
-          <p>{selectedRestaurant.description}</p>
-          {/* ... otras opciones y funcionalidades */}
-        </div>
-      )}
+    <div >
+      <TopBar showSearch={true} />
+      <div className='restaurant-favorites'>
+        <h2 className="restaurant-favorites-heading">Tus restaurantes favoritos</h2>
+        {favoriteRestaurants.length > 0 ? (
+          <div className="restaurant-favorites-list">
+            {favoriteRestaurants.map((restaurant, index) => (
+              <RestaurantCardFavorites
+              key={index}
+              name={restaurant}
+              image={restaurant.urlImg}
+              />
+            ))}
+          </div>
+        ) : (
+          <p>No tienes restaurantes favoritos.</p>
+        )}
+        {selectedRestaurant && (
+          <div className="restaurant-details">
+            {/* Mostrar información completa del restaurante */}
+            <h2>{selectedRestaurant.nombre}</h2>
+            <p>{selectedRestaurant.description}</p>
+            {/* ... otras opciones y funcionalidades */}
+          </div>
+        )}
+      </div>
       <Footer />
     </div>
   );
