8248df492cde3681a9521c8fcc09cab69db225b1
BrayanGalindez <bgalindez@unal.edu.co>
2023-06-25 23:33:37 -0500

    Update RestaurantCardFavorites.jsx

--- bogota_taste/src/components/Restaurant/RestaurantCardFavorites.jsx
+++ bogota_taste/src/components/Restaurant/RestaurantCardFavorites.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../../styles/Restaurant/RestaurantCard.css';
 
-const RestaurantCard = ({ name }) => {
+const RestaurantCardFavorites = ({ name, image, description, direccion, onViewMore, updateRestaurants  }) => {
   return (
     <div className="restaurant-card">
-      <h3>{name}</h3>
+      <img className='restaurant-card__image' src={image} alt={name} />
+      <h3 className='restaurant-card__name'>{name}</h3>
+      <p className='restaurant-card__description'>{description}</p>
+      <h3 className='restaurant-card__direccion'>{direccion}</h3>
+      <button className='restaurant-card__button' onClick={onViewMore}>Ver más</button>
     </div>
   );
 };
 
-export default RestaurantCard;
+export default RestaurantCardFavorites;
